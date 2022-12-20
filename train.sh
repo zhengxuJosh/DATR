1dd9df320d776726ca1f08f1e5daf91603e26830
XuZheng <49426295+zhengxuJosh@users.noreply.github.com>
2022-12-20 16:02:36 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 export cur_dir=`pwd`
-export run_file="train_syn22.py"
-export save_exp_name="1025_syn[1024_512]_sup_DATRS"
+export run_file="sup_syn22.py"
+export save_exp_name="syn_sup_DATRS"
 
 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=22020 ${run_file} --save_root="${cur_dir}/exp/${save_exp_name}/" \
 --lr=0.00005 --batch-size=4 --num_epochs=50 --backbone=DATRS \
