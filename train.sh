047f3dd5983e49ccbe5551e444e49cb54d2c5e67
XuZheng <49426295+zhengxuJosh@users.noreply.github.com>
2022-12-20 16:00:27 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,11 @@
+export cur_dir=`pwd`
+export run_file="train_syn22.py"
+export save_exp_name="1025_syn[1024_512]_sup_DATRS"
+
+python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=22020 ${run_file} --save_root="${cur_dir}/exp/${save_exp_name}/" \
+--lr=0.00005 --batch-size=4 --num_epochs=50 --backbone=DATRS \
+\
+--lamda 50 \
+2>&1 \
+| tee "log/${save_exp_name}.`date`"
+
